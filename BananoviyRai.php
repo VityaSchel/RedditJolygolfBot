6a490315e0737de8cb6f269870d89aceb402dea4
VityaSchel <vityaschel@gmail.com>
2020-10-09 15:51:18 +0300

    Added new source BananoviyRai (vkontakte group). Added pinned posts skip in BananoviyRai and NaPriemeUShevcova php scripts. I used Rai insted of Ray because Ray is an english word and may confuse someone.

--- BananoviyRai.php
+++ BananoviyRai.php
@@ -1,7 +1,8 @@
+
 <?php
 
 define('WORK_DIR', file_get_contents(dirname(__FILE__)."/secrets/work_dir.txt"));
-$sourceID = "-88245281";
+$sourceID = "-98340630";
 
 $resp = file_get_contents(file_get_contents(WORK_DIR."secrets/vk_api.txt").$sourceID);
 $resp = json_decode($resp, true);
@@ -14,33 +15,24 @@ if($resp['response']['items'][0]['is_pinned'] == 1){
 
 foreach($resp['response']['items'] as $item){
   $id = $item['id'];
-  $idlast = file_get_contents(WORK_DIR."NaPriemeUShevcova_last_posted_id.txt");
+  $idlast = file_get_contents(WORK_DIR."BananoviyRai_last_posted_id.txt");
   if($id == $idlast){
     die("Nothing to do");
   }
-  file_put_contents(WORK_DIR."NaPriemeUShevcova_last_posted_id.txt", $id);
+  file_put_contents(WORK_DIR."BananoviyRai_last_posted_id.txt", $id);
 
   if(count($item['copy_history']) > 0){
     die("Repost");
   }
-  if($item['marked_as_ads'] == "1"){
+  if(strpos($item['text'],"WASD") > -1){
     die("Ads");
   }
-  if(strpos($item['text'], "WASD") > -1){
+  if(strpos($item['text'],"wasd") > -1){
     die("Ads");
-  }
-  if(strpos($item['text'], "wasd") > -1){
-    die("Ads");
-  }
-  if(strpos($item['text'], "youtu.be/") > -1){
-    die("Possible duplicate: youtu.be");
-  }
-  if(count($item['copy_history']) > 0){
-    die("Repost");
   }
   $title = str_replace(";",",",$item['text']);
   if($title == ""){
-    $title = "На приёме у Шевцова";
+    $title = "Банановый Рай";
   }
   $attachs = $item['attachments'];
   $type = "";
@@ -73,11 +65,11 @@ foreach($resp['response']['items'] as $item){
   $comments = $item['comments']['count'];
   $views = $item['views']['count'];
   $lin = $item['from_id']."_".$item['id'];
-  file_put_contents(WORK_DIR."resources/data/NaPriemeUShevcova.txt", $type.';'.$image.';'.$likes.';'.$reposts.';'.$comments.';'.$views.';'.base64_encode($title).';'.$lin);
+  file_put_contents(WORK_DIR."resources/data/BananoviyRai.txt", $type.';'.$image.';'.$likes.';'.$reposts.';'.$comments.';'.$views.';'.base64_encode($title).';'.$lin);
   if($type == 'img'){
     $url = $image;
-    file_put_contents(WORK_DIR."resources/picture/NaPriemeUShevcova.jpg", file_get_contents($url));
+    file_put_contents(WORK_DIR.'resources/picture/BananoviyRai.jpg', file_get_contents($url));
   }
-  file_put_contents(WORK_DIR."logs/NaPriemeUShevcova_python.txt", shell_exec('python3 '.WORK_DIR.'NaPriemeUShevcova.py').PHP_EOL, FILE_APPEND);
+  file_put_contents(WORK_DIR."logs/BananoviyRai_python.txt", shell_exec('python3 '.WORK_DIR.'BananoviyRai.py').PHP_EOL, FILE_APPEND);
 }
 ?>
---
+++ BananoviyRai.py
@@ -0,0 +1,75 @@
+import praw
+import base64
+import re
+import sys
+
+work_dir = sys.argv[0].split("BananoviyRai.py")[0];
+
+client_id_f = open(work_dir+"secrets/reddit_client_id.txt", mode="r", encoding="utf-8")
+client_id_s = client_id_f.read();
+client_secret_f = open(work_dir+"secrets/reddit_client_secret.txt", mode="r", encoding="utf-8")
+client_secret_s = client_secret_f.read();
+password_f = open(work_dir+"secrets/reddit_password.txt", mode="r", encoding="utf-8")
+password_s = password_f.read();
+
+f = open(work_dir+"resources/data/BananoviyRai.txt", mode="r", encoding="utf-8")
+infs = f.read().split(';', 7);
+
+reddit = praw.Reddit(client_id=client_id_s,
+                     client_secret=client_secret_s,
+                     password=password_s,
+                     user_agent='utidteam.com bot by /u/vitya_schel',
+                     username='Jolygolf_bot')
+
+type = infs[0]
+originalTitle = base64.b64decode(infs[6]).decode('utf-8');
+leaveFullComment = 0
+if(type == 'img'):
+    if(len(originalTitle) < 300):
+        title = originalTitle;
+    else:
+        title = "Банановый Рай (текст записи в комментариях)";
+        leaveFullComment = 1
+    image = work_dir+'resources/picture/BananoviyRai.jpg'
+    submission = reddit.subreddit('jolygolf').submit_image(title, image)
+else:
+    if(type.split(":")[0] == "poll"):
+        title = 'Банановый Рай'
+        submission = reddit.subreddit('jolygolf').submit_poll(title, selftext= originalTitle, options=type.split(":")[1].split("#"), duration=3)
+        leaveFullComment = 0;
+    else:
+        title = 'Банановый Рай'
+        submission = reddit.subreddit('jolygolf').submit(title, selftext=originalTitle)
+        leaveFullComment = 0;
+
+likes = str(infs[2]);
+reposts = str(infs[3]);
+comments = str(infs[4]);
+views = str(infs[5]);
+
+source = "Банановый Рай"
+flairtext = "{source} | {l} :l: | {r} :r: | {v} :e:".format(source=source, l=likes, r=reposts, v=views)
+if(len(flairtext) > 64):
+    flairtext = "{source} |{l}:l: | {r}:r: | {v}:e:".format(source=source, l=likes, r=reposts, v=views)
+    if(len(flairtext) > 64):
+        source = "Банан"
+        flairtext = "{source} | {l}:l: | {r}:r: | {v}:e:".format(source=source, l=likes, r=reposts, v=views)
+
+submission.flair.select("2ea55bd8-e3e6-11ea-8a5c-0e7cceef0c57", flairtext)
+
+urlID = infs[7].split("_")[1]
+urlFull = "https://jolybot.utidteam.com/away.php?url={urlid}&source=bananoviyrai".format(urlid=urlID)
+if(leaveFullComment == 0):
+    comment = submission.reply("[Запись ВКонтакте]({urlFull})".format(urlFull=urlFull))
+else:
+    infs = originalTitle.splitlines()
+    txt = "";
+    for line in infs:
+        txt += '\n> {l}'.format(l=line)
+
+    txt = re.sub("(> \n)", "", txt);
+
+    comment = submission.reply("[Запись ВКонтакте]({urlFull}) \n\nПолный текст записи: {txt}".format(urlFull=urlFull, txt=txt))
+comment.mod.distinguish(how='yes', sticky=True)
+comment.mod.approve();
+submission.mod.approve();
