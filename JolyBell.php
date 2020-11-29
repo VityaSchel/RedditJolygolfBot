682493b3abb7631b8f2b38ca4064b18a885fa195
Viktor <vityaschel@utidteam.com>
2020-11-30 01:03:41 +0400

    Emoji to name added

--- JolyBell.php
+++ JolyBell.php
@@ -1,5 +1,5 @@
 <?php
 
-exec('php7.2 '.dirname(__FILE__).'/api_request.php --id -141715625 --sourcespec JolyBell --sourcename JolyBell --sourceshort JB --flairid="d6e6d32a-1033-11eb-ab9e-0e93b157e05d"');
+exec('php7.2 '.dirname(__FILE__).'/api_request.php --id -141715625 --sourcespec JolyBell --sourcename :j:JolyBell --sourceshort :j:JB --flairid="d6e6d32a-1033-11eb-ab9e-0e93b157e05d"');
 
 ?>
