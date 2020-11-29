c6950c53f3178d703d028420e4832fa5ab541107
Viktor <vityaschel@utidteam.com>
2020-11-30 01:01:22 +0400

    Emoji to name added

--- BananoviyRai.php
+++ BananoviyRai.php
@@ -1,5 +1,5 @@
 <?php
 
-exec('php7.2 '.dirname(__FILE__).'/api_request.php --id -98340630 --sourcespec BananoviyRai --sourcename Банановый_Рай --sourceshort Банан --flairid="ee89a926-1033-11eb-9b92-0e2d63a37eb9"');
+exec('php7.2 '.dirname(__FILE__).'/api_request.php --id -98340630 --sourcespec BananoviyRai --sourcename :b:Банановый_Рай --sourceshort :b:Банан --flairid="ee89a926-1033-11eb-9b92-0e2d63a37eb9"');
 
 ?>
